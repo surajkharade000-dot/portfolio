d3eb0fbf1dca3af874ff57ec0e8b085442bd6675
surajkharade000-dot <surajkharade000@gmail.com>
2025-10-18 17:11:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -78,6 +78,7 @@ function App() {
           <li>Email: <a href="surajkharade000@gmail.com" className="contact-link">surajkharade000@gmail.com</a></li>
           <li>Instagram: <a href="https://www.instagram.com/x.suraj__18?igsh=MjQ0MG43cmtsZmQw" target="_blank" rel="noopener noreferrer" className="contact-link">x.suraj__18</a></li>
           <li>youtube: <a href="https://youtube.com/@surajjediting?si=GE2jJcv8xB1yNi7q" target="_blank" rel="noopener noreferrer" className="contact-link">@surajjediting</a></li>
+          <li>Mobile no.: <a href="9767774450" target="_blank" rel="noopener noreferrer" className="contact-link">9767774450</a></li>
         </ul>
       </section>
 
